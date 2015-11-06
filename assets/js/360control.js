f803e0519e45ca636fb6309ccc6d85b6b5f50628
jhars <jjharlan06@gmail.com>
2015-11-05 22:08:07 -0800

    first live push

--- assets/js/360control.js
+++ assets/js/360control.js
@@ -4,7 +4,6 @@ var canvas = document.getElementById("clickCanvas");
 var ctx = canvas.getContext("2d");
 
 var image = new Image()
-// image.src = 'images/bay360Sprint.png';
 image.src = 'http://i.imgur.com/BfY4QW5.png';
 
 var frames = 100;
@@ -17,68 +16,69 @@ var height = 81.75;
 var B;
 var ALPHA;
 var DIFF = 0;
-
-// ctx.clearRect(0, 0, width, height);
 ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
-// ??WHy currentFram * height??
+
+
+
+// var iphoneMarker = document.getElementById("shot-iphone");
+// var ictx = iphoneMarker.getContext("2d");
+// var shotIphone = new Image()
+// shotIphone.src = './images/shotOnMyIphone.png';
+// console.log("img loades");
+
+
 
 
 function initControl() {
 	var X = [];
 	var DELTA; 
 
-	// var ALPHA2 =
 	canvas.addEventListener("mousedown", doMouseDown, false);
 
 	function doMouseDown (event) {
 		X[0] = event.pageX;
-		canvas.addEventListener("mousemove",mouseX, false);
-		//set initial X == currentMouse Location
-		
 
+
+
+
+		// ictx.drawImage(shotIphone, 69, 58);
+		// ictx.drawImage(shotIphone, 400, 400, 400, 400);
+		// console.log("img DRAWN");
+
+		canvas.addEventListener("mousemove",mouseX, false);
 		document.body.addEventListener("mouseup", mouseUp, false);
-		
-	};	
-	function mouseUp(event) {
-		// document.body.removeEventListener("mouseup", mouseUp, false);
-	  canvas.removeEventListener("mousemove",mouseX, false);
-	  console.log("alpha2 -> " + ALPHA2);
+
+
 	};
 
+	function mouseUp(event) {
+	  canvas.removeEventListener("mousemove",mouseX, false);
 
+	};
 
 	function mouseX (event) {
-	    // event.preventDefault();
 	    var canvasX = event.pageX;
 	    X[1] = canvasX;
-	    console.log("hey Dude");
-	    //code for marker Graphic Here.
+	  	A = ALPHA; DIFF = X[0] - X[1]; ALPHA = DIFF; B = ALPHA;
 
-	  	A = ALPHA;
-	  	DIFF = X[0] - X[1];
-	  	ALPHA = DIFF;
-	  	B = ALPHA;
+			if (B < A){//dragging RIGHT
+	  		if (currentFrame === frames-1 ) {
+	  			currentFrame = frames-1;//>> Limit Here (upper)
+	  		} else {
+	  			currentFrame++;
+	  		}
+	  	} 	
+	  	if (B > A) {//dragging LEFT
+	  		if (currentFrame < bottomFrame ) {//or TRY 1 here...> see helmetLoop
+	  			currentFrame = 18;//>> Limit Here (lower)
+	  		} 
+	  		currentFrame--;
+	  	} 
 
-	  			if (B < A){
-
-	  	  		if (currentFrame === frames ) {
-	  	  			currentFrame = 18;
-	  	  		} currentFrame++;
-	  	  	} console.log("currentFrame1 = " + currentFrame)
-	  	  	if (B > A) {
-	  	  		if (currentFrame < bottomFrame ) {//or TRY 1 here...> see helmetLoop
-	  	  			currentFrame = frames;
-	  	  		} currentFrame--;
-	  	  		
-	  	  	} 
-	  	  	var ALPHA2 = ALPHA;	
-	  	  	
-	  	  	ctx.clearRect(0, 0, width, height);
-	  	  	ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
-	  	  	
-	  	  	//WHATS THIS???
-	  	  	X[1] = X[0];////
-	  // }
+	  	var ALPHA2 = ALPHA;	
+	  	ctx.clearRect(0, 0, width, height);
+	  	ctx.drawImage(image, 0, height * currentFrame, width, height, 0, 0, width, height);
+	  	X[1] = X[0];
 
 	}
 
