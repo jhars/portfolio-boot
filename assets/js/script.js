c165698ef5b78d2f4b8543df97ffaf23efa787c8
jhars <jjharlan06@gmail.com>
2015-11-10 16:12:44 -0800

    cleaned up interactive and figured out close button on portfolio items

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,22 +105,22 @@ $(document).ready(function () {
 
 
 
-    var mapCanvas = document.getElementById('map-canvas');
-    var mapOptions = {
-        center: new google.maps.LatLng(24.909439, 91.833800),
-        zoom: 16,
-        scrollwheel: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions)
+    // var mapCanvas = document.getElementById('map-canvas');
+    // var mapOptions = {
+    //     center: new google.maps.LatLng(24.909439, 91.833800),
+    //     zoom: 16,
+    //     scrollwheel: false,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    // }
+    // var map = new google.maps.Map(mapCanvas, mapOptions)
 
-    var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(24.909439, 91.833800),
-            title:"Boots4 Office"
-        });
+    // var marker = new google.maps.Marker({
+    //         position: new google.maps.LatLng(24.909439, 91.833800),
+    //         title:"Boots4 Office"
+    //     });
 
         // To add the marker to the map, call setMap();
-        marker.setMap(map);
+        // marker.setMap(map);
 
     //google.maps.event.addDomListener(window, 'load', initialize);
 
